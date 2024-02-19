264dba7ddea6ff964f45bd17b92efd74296b3dda
Ayush Khanduri <113684157+ayushkhanduri74@users.noreply.github.com>
2024-02-19 10:03:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,15 @@ AutoCaptionBotV1 = pyrogram.Client(
 start_message = """
 <b>👋Hello {}</b>
 <b>I am an AutoCaption bot</b>
-<b>All you have to do is add me to your channel and I will show you my power</b>
-<b>@VJ_Botz</b>"""
+"""
 
-about_message = """
-<b>• Name : <a href=https://t.me/VJ_Botz>VJ AutoCaption</a></b>
-<b>• Developer : <a href=https://t.me/VJ_Botz>[VJ UPDATES]</a></b>
-<b>• Language : Python3</b>
-<b>• Library : Pyrogram v{version}</b>
-<b>• Updates : <a href=https://t.me/VJ_Botz>Click Here</a></b>
-<b>• Source Code : <a href=https://github.com/VJBots/VJ-AutoCaption-Bot>Click Here</a></b>"""
+# about_message = """
+# <b>• Name : <a href=https://t.me/VJ_Botz>VJ AutoCaption</a></b>
+# <b>• Developer : <a href=https://t.me/VJ_Botz>[VJ UPDATES]</a></b>
+# <b>• Language : Python3</b>
+# <b>• Library : Pyrogram v{version}</b>
+# <b>• Updates : <a href=https://t.me/VJ_Botz>Click Here</a></b>
+# <b>• Source Code : <a href=https://github.com/VJBots/VJ-AutoCaption-Bot>Click Here</a></b>"""
 
 @AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
 def start_command(bot, update):
@@ -34,10 +33,10 @@ def start_command(bot, update):
 def strat_callback(bot, update):
   update.message.edit(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
-@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("about"))
-def about_callback(bot, update): 
-  bot = bot.get_me()
-  update.message.edit(about_message.format(version=pyrogram.__version__, username=bot.mention), reply_markup=about_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+# @AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("about"))
+# def about_callback(bot, update): 
+#   bot = bot.get_me()
+#   update.message.edit(about_message.format(version=pyrogram.__version__, username=bot.mention), reply_markup=about_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
 @AutoCaptionBotV1.on_message(pyrogram.filters.channel)
 def edit_caption(bot, update: pyrogram.types.Message):
@@ -73,9 +72,6 @@ def get_file_details(update: pyrogram.types.Message):
 def start_buttons(bot, update):
   bot = bot.get_me()
   buttons = [[
-   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/VJ_Botz"),
-   pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
-   ],[
    pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
    ]]
   return pyrogram.types.InlineKeyboardMarkup(buttons)
